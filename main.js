d974ce670214d8ccf214d47bf2b690dba9db58f8
Duncan Walter <dwalter@barlingarts.com>
2019-09-22 14:23:39 -0400

    and now it packages

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function createWindow() {
   } else {
     indexPath = url.format({
       protocol: 'file:',
-      pathname: path.join(__dirname, 'dist', 'index.html'),
+      pathname: path.join(__dirname, 'out', 'index.html'),
       slashes: true,
     })
   }
